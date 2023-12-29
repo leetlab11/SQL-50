b42753bbd3c7a062b46260ac9115eaac8ea33658
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2023-12-28 18:30:38 -0600

    Update 00595-big-countries.sql

--- 00595-big-countries.sql
+++ 00595-big-countries.sql
@@ -3,3 +3,12 @@
 select name, population, area
 from World
 where area >= 3000000 or population >= 25000000
+
+
+-- apple- 3
+-- google- 2
+-- bloomberg- 2
+-- amazon- 2
+-- adobe- 3
+-- facebook- 3
+-- yahoo- 2
