202637fde2bb7a15312bce3143de48ae5a11bd32
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2023-12-28 18:32:49 -0600

    Update 01757-recyclable-and-low-fat-products.sql

--- 01757-recyclable-and-low-fat-products.sql
+++ 01757-recyclable-and-low-fat-products.sql
@@ -3,3 +3,16 @@
 select product_id
 from Products
 where low_fats = 'Y' and recyclable = 'Y'
+
+
+-- amazon- 19
+-- adobe- 10
+-- google- 8
+-- facebook- 3
+-- apple- 3
+-- microsoft- 3
+-- yahoo- 2
+-- bloomberg- 2
+
+
+
