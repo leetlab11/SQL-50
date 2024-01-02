56a7f5903795b5b0c50b8fbc1c025bbf38c361c6
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-02 14:00:39 -0600

    Update 34-01164-product-price-at-a-given-date.sql

--- 34-01164-product-price-at-a-given-date.sql
+++ 34-01164-product-price-at-a-given-date.sql
@@ -22,6 +22,27 @@ from Products
 group by 1
 having min(change_date) > '2019-08-16'
 
+--------------------------------------------------------------------------------------------------------------------------------
+
+-- same but without using rank
+    
+select product_id, new_price as price
+from Products
+where (product_id, change_date) in 
+    (
+    select product_id, max(change_date) as change_date
+    from Products
+    where change_date <= '2019-08-16'
+    group by product_id
+    )
+    
+union
+    
+select product_id, 10 as price
+from Products
+group by 1
+having min(change_date) > '2019-08-16'
+
 
 -- google- 2
 -- amazon- 4
