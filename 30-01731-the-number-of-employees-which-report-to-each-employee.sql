4635a74d248f84146e0e307c7ac8f7e13ab9a2fa
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-02 11:41:25 -0500

    Update 30-01731-the-number-of-employees-which-report-to-each-employee.sql

--- 30-01731-the-number-of-employees-which-report-to-each-employee.sql
+++ 30-01731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,1 +1,6 @@
-
+select m.reports_to, e.name, count(m.reports_to), avg(e.age)
+from Employees m
+left join Employees e
+on m.reports_to = e.employee_id
+where m.reports_to is not null
+group by m.reports_to
